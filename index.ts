a0a8e24dae73719e9b6bb52c8fa88402707103ab
Dmitry <d@kozlov.ru>
2025-06-13 04:52:47 +0700

    Hub: Added port argument

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
 import { Hub } from './hub'
-new Hub()
+let port: number | undefined
+const argv = process.argv.slice(2)
+while (true) {
+  const command = argv.shift()
+  if (!command) break
+  switch (command) {
+    case '-p':
+    case '--port':
+      port = Number(argv.shift())
+      break
+    default:
+      break
+  }
+}
+new Hub(port)
