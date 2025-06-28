5bc5a8c8cdaa5fb48be9eedcbba52eefc14b7ef3
Dmitry <d@kozlov.ru>
2025-06-28 13:49:38 +0300

    Added counter load balancer

--- src/load balancers.ts
+++ src/load balancers.ts
@@ -1,4 +1,4 @@
-import type { Service } from '../hub'
+import type { Service } from './hub'
 
 export interface LoadBalancer {
   next(): Service
@@ -18,6 +18,22 @@ export class RandomLoadBalancer implements LoadBalancer {
   remove(service: Service) {
     const index = this.services.findIndex(s => s.sender === service.sender)
     if (index === -1) return
+    this.services.splice(index, -1)
+  }
+}
+
+export class CounterLoadBalancer extends RandomLoadBalancer {
+  index = 0
+  next(): Service {
+    if (this.services.length === 0) throw 'api not available'
+    this.index += 1
+    if (this.index >= this.services.length) this.index = 0
+    return this.services[this.index]
+  }
+  remove(service: Service) {
+    const index = this.services.findIndex(s => s.sender === service.sender)
+    if (index === -1) return
+    if (index < this.index) this.index -= 1
     this.services.splice(index, -1)
   }
 }
