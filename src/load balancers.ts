2b30953d91dc24f7ebee9e2f6efbfa098b2913e7
Dmitry <d@kozlov.ru>
2025-06-28 14:08:37 +0300

    Load balancers now use outside arrays for easier replacement

--- src/load balancers.ts
+++ src/load balancers.ts
@@ -1,74 +1,73 @@
 import type { Service } from './hub'
 
 export interface LoadBalancer {
-  next(): Service
-  add(item: Service): void
-  remove(item: Service): void
+  next(services: Service[]): Service
+  add(services: Service[], item: Service): void
+  remove(services: Service[], item: Service): void
 }
 
 export class RandomLoadBalancer implements LoadBalancer {
-  services: Service[] = []
-  next(): Service {
-    if (this.services.length === 0) throw 'api not available'
-    return this.services[Math.floor(Math.random() * this.services.length)]
+  next(services: Service[]): Service {
+    if (services.length === 0) throw 'api not available'
+    return services[Math.floor(Math.random() * services.length)]
   }
-  add(service: Service) {
-    this.services.push(service)
+  add(services: Service[], service: Service) {
+    services.push(service)
   }
-  remove(service: Service) {
-    const index = this.services.findIndex(s => s.sender === service.sender)
+  remove(services: Service[], service: Service) {
+    const index = services.findIndex(s => s.sender === service.sender)
     if (index === -1) return
-    this.services.splice(index, -1)
+    services.splice(index, -1)
   }
 }
 
 export class CounterLoadBalancer extends RandomLoadBalancer {
   index = 0
-  next(): Service {
-    if (this.services.length === 0) throw 'api not available'
+  next(services: Service[]): Service {
+    if (services.length === 0) throw 'api not available'
     this.index += 1
-    if (this.index >= this.services.length) this.index = 0
-    return this.services[this.index]
+    if (this.index >= services.length) this.index = 0
+    return services[this.index]
   }
-  remove(service: Service) {
-    const index = this.services.findIndex(s => s.sender === service.sender)
+  remove(services: Service[], service: Service) {
+    const index = services.findIndex(s => s.sender === service.sender)
     if (index === -1) return
     if (index < this.index) this.index -= 1
-    this.services.splice(index, -1)
+    services.splice(index, -1)
   }
 }
 
 export class FirstAvailableLoadBalancer extends RandomLoadBalancer {
-  next(): Service {
-    const service = this.services.find(a => !a.sending)
+  next(services: Service[]): Service {
+    const service = services.find(a => !a.sending)
     if (!service) throw 'api not available'
     return service
   }
 }
 
 export class CounterAvailableLoadBalancer extends CounterLoadBalancer {
-  next(): Service {
-    if (this.services.length === 0) throw 'api not available'
+  next(services: Service[]): Service {
+    if (services.length === 0) throw 'api not available'
     let index = this.index + 1
-    if (index >= this.services.length) index = 0
-    for (let i = index; i < this.services.length; i += 1) {
-      if (!this.services[i].sending) {
+    if (index >= services.length) index = 0
+    for (let i = index; i < services.length; i += 1) {
+      if (!services[i].sending) {
         this.index = i
-        return this.services[i]
+        return services[i]
       }
     }
     for (let i = 0; i < index; i += 1) {
-      if (!this.services[i].sending) {
+      if (!services[i].sending) {
         this.index = i
-        return this.services[i]
+        return services[i]
       }
     }
     throw 'api not available'
   }
-  remove(service: Service) {
-    const index = this.services.findIndex(s => s.sender === service.sender)
+  remove(services: Service[], service: Service) {
+    const index = services.findIndex(s => s.sender === service.sender)
     if (index === -1) return
     if (index < this.index) this.index -= 1
-    this.services.splice(index, -1)
+    services.splice(index, -1)
   }
 }
